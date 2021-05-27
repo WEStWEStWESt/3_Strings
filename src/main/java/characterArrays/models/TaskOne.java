f9b236f7f877c10d1ef848fed96bd5cc701ed547
WESt <WESt>
2021-05-27 22:56:24 +0300

    trying to use OOP...beginning

--- src/main/java/characterArrays/models/TaskOne.java
+++ src/main/java/characterArrays/models/TaskOne.java
@@ -14,6 +14,7 @@ import java.util.regex.Pattern;
 public class TaskOne implements Task {
     int count = 0;
     String str = new One().s;
+
     @Override
     public Object call() throws Exception {
         Pattern pattern = Pattern.compile("[A-Z]");
@@ -27,26 +28,4 @@ public class TaskOne implements Task {
         System.out.println("Uppercase counter = " + count);
         return count;
     }
-    /* String inputString = "This is simple that contains phone number +380505055050 That's great.";
-        String pattern = "(\\d+)";
-
-        Pattern ptrn = Pattern.compile(pattern);
-        Matcher matcher = ptrn.matcher(inputString);
-
-        if(matcher.find()){
-            System.out.println("Phone number: " + matcher.group(0));
-        }else {
-            System.out.println("PHONE NUMBER NOT FOUND");
-        }
-        */
-
-
-
-
-
-
-
-
-
-
 }
