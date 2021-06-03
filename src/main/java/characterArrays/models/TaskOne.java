f35402638a11fca1f819d12622aef2c8545c3a0c
WESt <WESt>
2021-06-03 23:14:20 +0300

    not ready yet..

--- src/main/java/characterArrays/models/TaskOne.java
+++ src/main/java/characterArrays/models/TaskOne.java
@@ -1,4 +1,5 @@
 package characterArrays.models;
+
 import characterArrays.support.One;
 import interfaces.Task;
 
@@ -12,78 +13,52 @@ import java.util.regex.Pattern;
      */
 
 public class TaskOne implements Task {
-    private int count = 0;
-    private int[] countIndex = new int[5];
+    private int countUpp = 0;
     private String[] strArr = new One().strArr;
     private Pattern pattern = Pattern.compile("[a-z][A-Z]");
-    private String string;
-    private String[] varArr;
-    private ArrayList<String> upperArrayList = new ArrayList<>();
-    private char[] charArray = new char[50000];
+    private ArrayList<String> arrList = new ArrayList<String>();
 
     @Override
     public Object call() throws Exception {
-//        for (String varStr : strArr) {
-//            Matcher matcher = pattern1.matcher(varStr);
-//
-//            while (matcher.find()) {
-//                if (!matcher.find()) {
-//                    count++;
-//                    System.out.println(" !find(varStr): " + varStr);
-//
-//                    string = varStr.toLowerCase();
-//                    System.out.println("toLowerCase " + string);
-//                   Matcher matcher1 = pattern2.matcher(string.toLowerCase());
-//
-//                } else {
-//                    System.out.println(" group: " + matcher);
-//                }
-//            }
-//        }
 
         for (int i = 0; i < strArr.length; i++) {
-            string = strArr[i];
+            String string = strArr[i];
             Matcher matcher = pattern.matcher(string);
-                if (matcher.find()) {
-                    int index = i;
-                    count++;
 
-                    upperArrayList.add(string);
-                    charArray = string.toCharArray();
-                    for (int j = 0; j < charArray.length; j++) {
-                        System.out.print(charArray[j] + "  ");
-                    }
-                    //System.out.println(index);
-                    System.out.println();
-                } else {
-                    System.out.println( "(" + (i) + ")element: "  + " String without UPPERCASE letters. ");
-                }
+            if (matcher.find()) {
+                countUpp = i;
 
+                char[] charArray = string.toCharArray();
+                arrList.add(countUpp, converter(charArray).toLowerCase());
+
+                System.out.println();
+            } else {
+                arrList.add(i, string);
+                System.out.println("(" + (i) + ")element: " + " String without UPPERCASE letters. ");
+            }
         }
-
-//        if (count > 0){        }
-        System.out.println();
-        System.out.println("upperArrayList" + upperArrayList);
-        System.out.println("Uppercase counter = " + count);
-        return count;
+        System.out.println("arrList" + arrList);
+        //System.out.println("Uppercase counter = " + countUpp);
+        return countUpp;
     }
 
+    private String converter(char[] coincidenceArr) {
+        char[] upChars = new char[coincidenceArr.length];
+        int countUpChar = 0;
+        int countLowChar = 0;
 
+        for (int i = 0; i < upChars.length; i++) {
 
-//    private void arrMethod (String str){
-//        for (String s : strArr) {
-//
-//        }
-
-
-            /*
-                split(String regex) - делит строку на массив по какому-то правилу
-                charAt(int index) - возвращает символ по указанному индексу
-                toUpperCase(char ch) - переводит в верхний регистр
-                substring(int beginIndex) - возвращает подстроку, начиная с указанного индекса
-                */
-
-
-//    }
-
+            if (Character.isUpperCase(coincidenceArr[i])) {
+                countUpChar = i;
+                upChars[countUpChar - 1] = '_';
+                System.out.print(" isUp: " + upChars[countUpChar - 1] + "  ");
+            } else {
+                countLowChar = i;
+                upChars[countLowChar] = coincidenceArr[i];
+                System.out.print(" * ");
+            }
+        }
+        return String.valueOf(upChars);
+    }
 }
